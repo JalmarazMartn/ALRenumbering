d64a8a20ef730a303b7daa497d2157740ac46b06
Jesus Almaraz martín <JalmarazMartn@gmail.com>
2022-07-30 08:11:35 +0200

    Usage category

--- src/setUsageCatagory.js
+++ src/setUsageCatagory.js
@@ -7,31 +7,30 @@ let menuNode = {
 }
 let menuNodeList = [];
 module.exports = {
-    setUsageCategory: function()
-    {
+    setUsageCategory: function () {
         processMenuTxtfile();
     }
 }
-    
+
 async function processMenuNodes(menuNodeList) {
     let OutputChannel = vscode.window.createOutputChannel('Set Usage Category');
     OutputChannel.clear();
-	OutputChannel.show();
+    OutputChannel.show();
 
-	const AllDocs = await vscode.workspace.findFiles('**/*.{al}');
-	for (let index = 0; index < AllDocs.length; index++) {
-		var ALDocumentURI = AllDocs[index];
-		var ALDocument = await vscode.workspace.openTextDocument(ALDocumentURI);
-		var DeclarationLineText = await GetDeclarationLineText(ALDocument);
-		OutputChannel.appendLine('Processing ' + DeclarationLineText);	
+    const AllDocs = await vscode.workspace.findFiles('**/*.{al}');
+    for (let index = 0; index < AllDocs.length; index++) {
+        var ALDocumentURI = AllDocs[index];
+        var ALDocument = await vscode.workspace.openTextDocument(ALDocumentURI);
+        var DeclarationLineText = await GetDeclarationLineText(ALDocument);
+        OutputChannel.appendLine('Processing ' + DeclarationLineText);
         let CurrentObjectFromLineText = GetCurrentObjectFromLineText(DeclarationLineText);
-		let menuNode = menuNodeList.find(x => x.Id === CurrentObjectFromLineText.ObjectID
-                && x.Type === CurrentObjectFromLineText.ObjectType);
-		if (menuNode) {
+        let menuNode = menuNodeList.find(x => x.Id === CurrentObjectFromLineText.ObjectID
+            && x.Type === CurrentObjectFromLineText.ObjectType);
+        if (menuNode) {
             await appendUsageinDocument(ALDocument, menuNode.UsageCategory);
             OutputChannel.appendLine(DeclarationLineText + ' With usage');
-		}
-	};
+        }
+    };
 
 }
 
@@ -41,18 +40,17 @@ function GetDeclarationLineText(ALDocument) {
 }
 function GetDeclarationLineNumber(ALDocument) {
     const Library = require('./Library.js');
-    return Library.GetDeclarationLineNumber(ALDocument);    
+    return Library.GetDeclarationLineNumber(ALDocument);
 }
 
-async function appendUsageinDocument(ALDocument, UsageCategory) {    
+async function appendUsageinDocument(ALDocument, UsageCategory) {
     const WSEdit = new vscode.WorkspaceEdit;
     const DeclarationLineNumber = GetDeclarationLineNumber(ALDocument);
-    const PositionOpen = new vscode.Position(DeclarationLineNumber , 0);
+    const PositionOpen = new vscode.Position(DeclarationLineNumber, 0);
     await WSEdit.insert(ALDocument.uri, PositionOpen, 'UsageCategory = "' + menuNode.UsageCategory + '"');
-    await vscode.workspace.applyEdit(WSEdit);    
+    await vscode.workspace.applyEdit(WSEdit);
 }
-async function processMenuTxtfile()
-{
+async function processMenuTxtfile() {
     const fs = require('fs');
     const filePath = await vscode.window.showOpenDialog({
         canSelectFiles: true,
@@ -60,19 +58,32 @@ async function processMenuTxtfile()
         canSelectMany: false,
         openLabel: 'Select Menu.txt file'
     });
-    const menuTxtFile = fs.readFileSync(filePath[0].fsPath, 'utf8');
+    let menuTxtFile = fs.readFileSync(filePath[0].fsPath, 'utf8');
 
-    const itemRegex = /RunObjectType=(\w+);RunObjectID=(\d+);.+DepartmentCategory=(\w+)/gmi;
-    menuTxtFile.replace(/\\n/gm, '');
-    let match = itemRegex.exec(menuTxtFile);
-    while (match != null) {
-        menuNode = {
-            Type: match[1],
-            Id: match[2],
-            UsageCategory: match[3]
+    //const itemRegex = /RunObjectType=(\w+);RunObjectID=(\d+);.+DepartmentCategory=(\w+)/gmi;
+    const itemRegex1 = /RunObjectType=(\w+)/gmi;
+    const itemRegex2 = /RunObjectID=(\d+)/gmi;
+    const itemRegex3 = /DepartmentCategory=(\w+)/gmi;
+    let menuLines = menuTxtFile.split('\r\n');
+    menuNodeList = [];
+    for (let index = 0; index < menuLines.length; index++) {
+        let match3 = itemRegex3.exec(menuLines[index]);
+        if (match3) {
+            menuNode.UsageCategory = match3[1];
+            menuNodeList.push({
+                Type: menuNode.Type,
+                Id: menuNode.Id,
+                UsageCategory: menuNode.UsageCategory
+            });
         }
-        menuNodeList.push(menuNode);
-        match = itemRegex.exec(menuTxtFile);
+        let match2 = itemRegex2.exec(menuLines[index]);
+        if (match2) {
+            menuNode.Id = match2[1];
+        }
+        let match1 = itemRegex1.exec(menuLines[index]);
+        if (match1) {
+            menuNode.Type = match1[1];
+        }
     }
     console.log(menuNodeList);
 }
